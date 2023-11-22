fb45d56783621c9592580c66e106908beb7e3e55
Alex Brik <g00dv1n.private@gmail.com>
2023-11-22 21:25:56 +0200

    Simplify examples and upd version

--- examples/contract_type.rs
+++ examples/contract_type.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 use ethers::{
     providers::{Http, Middleware, Provider},
     types::Address,
@@ -13,7 +11,7 @@ use eyre::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     let provider = Provider::<Http>::try_from("https://rpc.flashbots.net/fast")?;
-    let token_addr = Address::from_str("0xdac17f958d2ee523a2206206994597c13d831ec7")?;
+    let token_addr: Address = "0xdac17f958d2ee523a2206206994597c13d831ec7".parse()?;
 
     let code = provider.get_code(token_addr, None).await?;
 
